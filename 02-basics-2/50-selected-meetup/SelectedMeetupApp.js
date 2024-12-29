dd8371eee6ec44b3ebd1de8c5c73ec54bfaba5a5
kxxdree <truebackward@gmail.com>
2024-12-29 20:37:27 +0300

    feat: add selected-meetup task

--- 02-basics-2/50-selected-meetup/SelectedMeetupApp.js
+++ 02-basics-2/50-selected-meetup/SelectedMeetupApp.js
@@ -1,78 +1,74 @@
-import { defineComponent } from 'vue'
-// import { getMeetup } from './meetupsService.ts'
+import { defineComponent, onMounted, ref, watch } from 'vue'
+import { getMeetup } from './meetupsService.ts'
 
 export default defineComponent({
   name: 'SelectedMeetupApp',
 
-  setup() {},
+  setup() {
+    const pageNumber = ref(1)
+    const buttons = ref([1, 2, 3, 4, 5])
+    const meetup = ref({})
+
+    const fetchMeetup = async id => {
+      meetup.value = await getMeetup(id)
+    }
+
+    onMounted(async () => {
+      await fetchMeetup(pageNumber.value)
+    })
+
+    watch(pageNumber, async newValue => {
+      await fetchMeetup(newValue)
+    })
+
+    const pickNextMeetup = () => {
+      if (pageNumber.value < 5) {
+        pageNumber.value++
+      }
+    }
+
+    const pickPrevMeetup = () => {
+      if (pageNumber.value > 1) {
+        pageNumber.value--
+      }
+    }
+
+    return {
+      pageNumber,
+      buttons,
+      meetup,
+      pickNextMeetup,
+      pickPrevMeetup,
+    }
+  },
 
   template: `
     <div class="meetup-selector">
       <div class="meetup-selector__control">
-        <button class="button button--secondary" type="button" disabled>Предыдущий</button>
+        <button class="button button--secondary" type="button" :disabled='pageNumber === 1' @click='pickPrevMeetup'>Предыдущий</button>
 
         <div class="radio-group" role="radiogroup">
-          <div class="radio-group__button">
+          <div v-for='button in buttons' class="radio-group__button">
             <input
-              id="meetup-id-1"
+              :id="'meetup-id-' + button"
               class="radio-group__input"
               type="radio"
               name="meetupId"
-              value="1"
+              :value="button"
+              v-model='pageNumber'
             />
-            <label for="meetup-id-1" class="radio-group__label">1</label>
-          </div>
-          <div class="radio-group__button">
-            <input
-              id="meetup-id-2"
-              class="radio-group__input"
-              type="radio"
-              name="meetupId"
-              value="2"
-            />
-            <label for="meetup-id-2" class="radio-group__label">2</label>
-          </div>
-          <div class="radio-group__button">
-            <input
-              id="meetup-id-3"
-              class="radio-group__input"
-              type="radio"
-              name="meetupId"
-              value="3"
-            />
-            <label for="meetup-id-3" class="radio-group__label">3</label>
-          </div>
-          <div class="radio-group__button">
-            <input
-              id="meetup-id-4"
-              class="radio-group__input"
-              type="radio"
-              name="meetupId"
-              value="4"
-            />
-            <label for="meetup-id-4" class="radio-group__label">4</label>
-          </div>
-          <div class="radio-group__button">
-            <input
-              id="meetup-id-5"
-              class="radio-group__input"
-              type="radio"
-              name="meetupId"
-              value="5"
-            />
-            <label for="meetup-id-5" class="radio-group__label">5</label>
+            <label :for="'meetup-id-' + button" class="radio-group__label">{{ button }}</label>
           </div>
         </div>
 
-        <button class="button button--secondary" type="button">Следующий</button>
+        <button class="button button--secondary" type="button" :disabled='pageNumber === 5' @click='pickNextMeetup'>Следующий</button>
       </div>
 
       <div class="meetup-selector__cover">
         <div class="meetup-cover">
-          <h1 class="meetup-cover__title">Some Meetup Title</h1>
+          <h1 class="meetup-cover__title">{{ meetup.title }}</h1>
         </div>
       </div>
-
     </div>
   `,
 })
